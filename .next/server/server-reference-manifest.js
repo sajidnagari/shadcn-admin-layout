9256584d629b99a72724c72bfa7a3d73d1c82fe7
sajidnagari <sajidnagari5@gmail.com>
2024-11-08 19:56:22 +0300

    remove unused code

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"nyia/xd/OdXh63EJjrIqUTRLe+zk4xWwVgz1RpfYzDE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"G7Y2//WtnB3atyMjfAfb9+NWNxbpq8VF5eU/BtxMemU=\"\n}"
