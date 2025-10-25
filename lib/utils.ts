6b973d64485d136ab6170cfb56e178f791379f80
sajidnagari <sajidnagari5@gmail.com>
2025-10-26 01:23:51 +0500

    updated

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,3 +5,11 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+
+export function getEnv(key: string) {
+  const value = process.env[key]
+  if (!value) {
+    throw new Error(`Environment variable ${key} is not set`)
+  }
+  return value
+}
